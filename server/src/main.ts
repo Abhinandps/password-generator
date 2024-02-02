e6d8f00bc8214e86392376f9b663f4f54031ec37
AbhinandPS <psabhinand333s@gmail.com>
2024-02-02 13:27:43 +0530

    uri

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,9 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://react-password-generator-umber.vercel.app/'
+    origin: 'https://react-password-generator-umber.vercel.app'
   })
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
 }
 bootstrap();
+
+
