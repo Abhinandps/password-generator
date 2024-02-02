61d5f24fe70d894167b407bed0512678048435f7
AbhinandPS <psabhinand333s@gmail.com>
2024-02-02 13:30:36 +0530

    cors updated test

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://react-password-generator-umber.vercel.app'
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Content-Length'], 
+    credentials: true, 
   })
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
