0683ff220fbc93030b117461fb280e662fb6f10a
AbhinandPS <psabhinand333s@gmail.com>
2024-02-02 13:39:09 +0530

    CORS policy restrictions

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,11 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    exposedHeaders: ['Content-Length'], 
-    credentials: true, 
+    origin: 'https://react-password-generator-umber.vercel.app',
   })
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
